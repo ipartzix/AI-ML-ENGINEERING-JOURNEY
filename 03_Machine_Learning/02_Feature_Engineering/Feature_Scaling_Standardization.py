cb86b0062b52b5ef287caa96c84320541ddd86f1
ipartzix <parthaPaul2705@gmail.com>
2026-03-06 19:21:01 +0530

    Refactor (GE):refactor code structure for improved readability and maintainability

---
+++ 03_Machine_Learning/02_Feature_Engineering/Feature_Scaling_Standardization.py
@@ -0,0 +1,5 @@
+import pandas as pd  # data processing
+
+df = pd.read_csv(
+    "https://raw.githubusercontent.com/campusx-official/100-days-of-machine-learning/refs/heads/main/day24-standardization/Social_Network_Ads.csv")
+print(df)
