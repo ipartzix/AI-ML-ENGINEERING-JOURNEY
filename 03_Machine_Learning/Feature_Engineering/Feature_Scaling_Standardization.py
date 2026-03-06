759c32710ddfbb8c275be19004b0a3b93aa1e316
ipartzix <parthaPaul2705@gmail.com>
2026-03-06 19:20:30 +0530

    chore(FE): remove obsolete feature engineering documentation and related scripts

--- 03_Machine_Learning/Feature_Engineering/Feature_Scaling_Standardization.py
+++
@@ -1,5 +0,0 @@
-import pandas as pd  # data processing
-
-df = pd.read_csv(
-    "https://raw.githubusercontent.com/campusx-official/100-days-of-machine-learning/refs/heads/main/day24-standardization/Social_Network_Ads.csv")
-print(df)
