45631510d2533b23b012bb3ebd82f225a920bf3b
Partha Paul <parthapaul2705@gmail.com>
2026-03-29 21:46:38 +0530

    feat(FE):so print top 10 rows

--- 03_Machine_Learning/02_Feature_Engineering/Feature_Scaling_Standardization.py
+++ 03_Machine_Learning/02_Feature_Engineering/Feature_Scaling_Standardization.py
@@ -3,3 +3,4 @@ import pandas as pd  # data processing
 df = pd.read_csv(
     "https://raw.githubusercontent.com/campusx-official/100-days-of-machine-learning/refs/heads/main/day24-standardization/Social_Network_Ads.csv")
 print(df)
+print(df.head(10))
