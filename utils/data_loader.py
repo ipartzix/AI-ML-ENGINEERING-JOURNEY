feb5d9f976b10cb693029eb53775a1f8862daf19
Partha Paul <parthapaul2705@gmail.com>
2026-04-17 06:18:34 +0530

    feat(utils):update and structured  the  load

--- utils/data_loader.py
+++ utils/data_loader.py
@@ -4,18 +4,14 @@ import pandas as pd
 def load_csv(file_path: str) -> pd.DataFrame:
     """
     Load a CSV file into a pandas DataFrame.
-    
-
     Args:
         file_path (str): Path to the CSV file.
-        
-
     Returns:
         pd.DataFrame: Loaded dataset.
     """
-    
     try:
         df = pd.read_csv(file_path)
+
         print(f"Data loaded successfully. Shape: {df.shape} - data_loader.py:16")
         return df
     except Exception as e:
