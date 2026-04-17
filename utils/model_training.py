e0635429e45559831eb95289eb9a009a719eccf4
Partha Paul <parthapaul2705@gmail.com>
2026-04-17 06:29:23 +0530

    feat(utils):update and structured  of the model training

--- utils/model_training.py
+++ utils/model_training.py
@@ -1,7 +1,8 @@
 from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import accuracy_score
 
+from sklearn.linear_model import LogisticRegression
+
+from sklearn.metrics import accuracy_score
 
 def train_model(X, y):
     """
