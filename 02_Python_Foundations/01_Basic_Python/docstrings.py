2d54339afa29532bde25615dc8f64f0e34ede097
Partha Paul <parthapaul2705@gmail.com>
2026-06-01 20:24:13 +0530

    feat(py): update file proper structure

--- 02_Python_Foundations/01_Basic_Python/docstrings.py
+++ 02_Python_Foundations/01_Basic_Python/docstrings.py
@@ -2,6 +2,4 @@ def sum(a, b):
     '''This will sum two numbers this note for a proper example of comment it just pest the comment """ …""" in the next line'''
     c = a + b
     return c
-
-
 print(sum.__doc__)
