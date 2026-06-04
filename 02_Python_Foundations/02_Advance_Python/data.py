2e5c4d924d5c6e0e2a83d246105dd169c40fd211
Partha Paul <parthapaul2705@gmail.com>
2026-06-04 19:10:28 +0530

    chore(python): refactor code and spaces

--- 02_Python_Foundations/02_Advance_Python/data.py
+++ 02_Python_Foundations/02_Advance_Python/data.py
@@ -3,16 +3,13 @@ sm= "sum"
 print(num)
 print(sm)
 
-
 a ='😶'
 print (ord(a))
 print(chr(ord(a)))
 
-
 A ="hello brother"
 print(A[8]) # positive indexing
 print(A[-6]) # negative indexing
-
 
 print(A[0:2:1]) # slice of code
 print(A[0::2])
