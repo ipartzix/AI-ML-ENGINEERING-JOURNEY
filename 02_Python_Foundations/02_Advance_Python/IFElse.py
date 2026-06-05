feff13f235d6e01b109c3225a8c60d2bb544afad
Partha Paul <parthapaul2705@gmail.com>
2026-06-05 12:18:32 +0530

    chore(python): refactor code and spaces

--- 02_Python_Foundations/02_Advance_Python/IFElse.py
+++ 02_Python_Foundations/02_Advance_Python/IFElse.py
@@ -1,13 +1,18 @@
 # print("Welcome to Python If else statement question ")
 # age =int (input("Enter a number: "))
+
 # if age >= 100:
 #     print("take rest ")
+
 # elif 19<= age >= 60:
 #     print("Your age is greater than or equal to 60")
+
 # elif 59>= age >=18:
 #     print("Your age is greater than or equal to 18")
+
 # else :
 #     print("Your age is less than or equal to 18")
+
 
 print("Welcome to Python If-Else Logic")
 age = int(input("Enter a number: "))
